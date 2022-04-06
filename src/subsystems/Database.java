dad7451a3337e6a13eb6176805f92ad33eba98be
Alg3bra1c <iasueltenfuss@gvtc.com>
2022-04-06 14:45:50 -0500

    Added comments to Database.java

--- src/subsystems/Database.java
+++ src/subsystems/Database.java
@@ -12,6 +12,11 @@ public class Database {
     private final String databaseUserName = "system";
     private final String databasePassword = "password";
 
+    /**
+     * Initializes the database connections
+     * @throws ClassNotFoundException
+     * @throws SQLException
+     */
     public Database() throws ClassNotFoundException, SQLException {
         Class.forName("oracle.jdbc.driver.OracleDriver");
         connection = DriverManager.getConnection(databaseURL, databaseUserName, databasePassword);
@@ -19,11 +24,49 @@ public class Database {
 
     }
 
-    public void insertData(String email, String firstName, String lastName, String itemModel, String RSI, String campus, String checkoutTime, String currentDeviceHealth, int expectedDuration) throws SQLException {
-        statement.executeUpdate("INSERT INTO Checkouts VALUES (" + email + ", " + firstName + ", " + lastName + ", " + itemModel + ", " + ", " + RSI + ", " + campus + ", " + checkoutTime
+    /**
+     * Inserts user inputted data into the CurrentCheckouts database
+     * @param email
+     * @param firstName
+     * @param lastName
+     * @param itemModel
+     * @param RSI
+     * @param campus
+     * @param checkoutTime
+     * @param currentDeviceHealth
+     * @param expectedDuration
+     * @throws SQLException
+     */
+    public void insertCurrentCheckoutData(String email, String firstName, String lastName, String itemModel, String RSI, String campus, String checkoutTime, String currentDeviceHealth, int expectedDuration) throws SQLException {
+        statement.executeUpdate("INSERT INTO CurrentCheckouts VALUES (" + email + ", " + firstName + ", " + lastName + ", " + itemModel + ", " + ", " + RSI + ", " + campus + ", " + checkoutTime
          + ", " + currentDeviceHealth + ", " + expectedDuration + ")");
     }
 
+    /**
+     * Inserts user inputted data into the PastCheckouts database
+     * @param email
+     * @param firstName
+     * @param lastName
+     * @param itemModel
+     * @param RSI
+     * @param campus
+     * @param checkoutTime
+     * @param initialDeviceHealth
+     * @param endingDeviceHealth
+     * @param expectedDuration
+     * @param renewals
+     * @param overdue
+     * @throws SQLException
+     */
+    public void insertPastCheckoutData(String email, String firstName, String lastName, String itemModel, String RSI, String campus, String checkoutTime, String initialDeviceHealth, String endingDeviceHealth, int expectedDuration, int renewals, boolean overdue) throws SQLException {
+        statement.executeUpdate("INSERT INTO PastCheckouts VALUES (" + email + ", " + firstName + ", " + lastName + ", " + itemModel + ", " + ", " + RSI + ", " + campus + ", " + checkoutTime
+         + ", " + initialDeviceHealth + ", " + endingDeviceHealth + ", " + expectedDuration + ", " + renewals + ", " + overdue + ")");
+    }
+
+    /**
+     * Closes the database connection
+     * @throws SQLException
+     */
     public void closeDatabase() throws SQLException {
         connection.close();
     }

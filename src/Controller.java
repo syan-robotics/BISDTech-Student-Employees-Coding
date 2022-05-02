0bd5db9b93da4cfa91530cb1c6feab52565fbad6
Yooser0 <crchampie@gmail.com>
2022-05-02 14:39:58 -0500

    Reminder format and testing reminding in Controller

--- src/Controller.java
+++ src/Controller.java
@@ -1,9 +1,20 @@
+import java.sql.SQLException;
+
+import javax.swing.JFrame;
+
 import subsystems.*;
 
 public class Controller {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        //Database database = new Database();
         Emailer emailer = new Emailer();
-        Notifier notify = new Notifier(emailer);
-        notify.remind("austinws43@boerneisd.net");
+        Notifier reminder = new Notifier(emailer);
+        reminder.remind("jerry.flores@boerneisd.net", 5, 2, 2022, "hp laptop");
+        //Coded from Jerry
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setVisible(true);
+
     }
+
 }
--- src/subsystems/Notifier.java
+++ src/subsystems/Notifier.java
@@ -1,11 +1,19 @@
 package subsystems;
+import java.util.GregorianCalendar;
 
 public class Notifier {
     Emailer emailer;
+
     public Notifier(Emailer emailer) {
         this.emailer = emailer;
     }
-    public void remind(String recipient) {
-        emailer.email(recipient,"IMPORTANT REMINDER","You're awesome");
+    public void remind(String recipient, int month, int day, int year, String itemName) {
+        
+        emailer.email(recipient, 
+        "BoerneISD Technology Department Reminder", 
+        String.format("Reminder! You loaned an item from the Boerne ISD Tech department on %d/%d/%d. Please return the %s as soon as you can. Thank you.", 
+            month, day, year, itemName));
+
     }
+
 }
